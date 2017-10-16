a2d2461ad6f948dd9e4c712a324cc96ebcb6a5d3
gmfjlr <32843776+gmfjlr@users.noreply.github.com>
2017-10-16 13:40:11 -0500

    Create hamburger-menu.js

---
+++ docs/assets/js/hamburger-menu.js
@@ -0,0 +1,3 @@
+/* Copyright (c) 2017 GMF */
+var hamburgerMenu = openMenu("hamburger-menu");
+function openMenu(id) {  var element = document.getElementById(id);  // var elementLeft = document.defaultView.getComputedStyle(element, null).left;  return function() {    with (element.style) {      left = (left === "0px") ? "-284px" : "0px";    }  };}

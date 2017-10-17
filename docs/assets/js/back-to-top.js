084043f4237dde7c6b4b886281d9bb1f826fe16a
YUKWAR <laroche2002special@gmail.com>
2017-10-16 21:23:06 -0500

    no message

--- docs/assets/js/back-to-top.js
+++ docs/assets/js/back-to-top.js
@@ -1,1 +1,6 @@
-window.onload = function() {  var elevator = new Elevator({    element: document.querySelector('#back-to-top'),    duration: 1000  });}
+window.onload = function() {
+  var elevator = new Elevator({
+    element: document.querySelector('#back-to-top'),
+    duration: 1000
+  });
+}
